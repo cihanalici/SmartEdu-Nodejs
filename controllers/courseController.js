b35caea6324204f40e5667edd333b112317c6e49
Deadburn <cihaom@gmail.com>
2022-06-03 20:38:06 +0300

    slug

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,9 @@
 const Course = require("../models/Course");
+const Category = require("../models/Category");
 
 exports.createCourse = async (req, res) => {
-  
   try {
-    const course = await Course.create(req.body);  
+    const course = await Course.create(req.body);
 
     res.status(201).json({
       status: "success",
@@ -18,20 +18,50 @@ exports.createCourse = async (req, res) => {
   }
 };
 
+exports.getCourse = async (req, res) => {
+
+  try {
+    const course = await Course.findOne({ slug: req.params.slug });
+
+    res.status(200).render("course", {
+      course,
+      page_name: "courses",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
 exports.getAllCourses = async (req, res) => {
-  
-    try {
-      const courses = await Course.find();
-      
-      res.status(200).render("courses", {
-          page_name: "courses",
-          courses
-      });
-      // res.send("Yeni Kurs Oluşturuldu.")
-    } catch (error) {
-      res.status(400).json({
-        status: "failure",
-        error,
-      });
+  try {
+
+    const categorySlug = req.query.categories;
+
+    const category = await Category.findOne({ slug: categorySlug})
+
+    let filter = {};
+
+    if(categorySlug) {
+      filter = {category: category._id}
     }
-  };
+
+    const courses = await Course.find(filter);
+
+    const categories = await Category.find();
+
+    res.status(200).render("courses", {
+      page_name: "courses",
+      courses,
+      categories
+    });
+    // res.send("Yeni Kurs Oluşturuldu.")
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error,
+    });
+  }
+};

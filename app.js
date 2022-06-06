1ecd33d4180892659c1863d64141329f9771c9f1
Deadburn <cihaom@gmail.com>
2022-06-06 21:00:13 +0300

    Session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require('./routes/userRoute')
+const session = require("express-session")
 
 const app = express();
 
@@ -23,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute)
+app.use("/users", userRoute)
 
 const port = 3000;
 app.listen(port, () => {
---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require('../controllers/authController')
+
+const router = express.Router();
+
+router.route('/signup').post(authController.createUser); //users/signup
+router.route('/login').post(authController.loginUser); //users/login
+
+
+module.exports = router;

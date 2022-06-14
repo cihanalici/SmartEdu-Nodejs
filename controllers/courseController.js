e7a5ffc306881b56368b846f6f56321915d9da97
Deadburn <cihaom@gmail.com>
2022-06-14 20:28:49 +0300

    course-student relation

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require("../models/Course");
 const Category = require("../models/Category");
+const User = require("../models/User");
 
 exports.createCourse = async (req, res) => {
   try {
@@ -63,6 +64,23 @@ exports.getAllCourses = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: "failure",
+      error,
+    });
+  }
+};
+
+
+exports.enrollCourse = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
       error,
     });
   }
--- models/User.js
+++ models/User.js
@@ -21,9 +21,9 @@ const UserSchema = new Schema({
         enum:["student", "teacher", "admin"],
         default: "student"
     },
-    courses: [{
+    courses:[{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course'
+        ref:'Course'
     }]
 });
 

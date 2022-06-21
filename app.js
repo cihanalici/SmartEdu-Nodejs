fc96c1eed6f7709a34cc330656c4687b2cbf69e0
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-06-21 11:26:31 +0300

    make it better

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 // Connect DB
-mongoose.connect('mongodb://0.0.0.0:27017/smartedu-db')
+mongoose.connect('mongodb+srv://cihan:Deadburn00.@cluster0.nsgfb.mongodb.net/smartedu-db?retryWrites=true&w=majority')
     .then(() => {
         console.log('DB Connected Successfuly');
     })
@@ -71,7 +71,7 @@ app.use('/users', userRoute)
 
 
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`App started on port ${port}`);
 })
